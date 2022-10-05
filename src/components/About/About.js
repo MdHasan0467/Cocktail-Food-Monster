904818a54ff39d32720c22085d4f88442b9b4959
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-10-06 01:32:51 +0600

    bepok edit korsi

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,11 +9,11 @@ const About = () => {
             <img className='sm:w-[30%] sm:h-[40%] w-[100%] h-[50%] ' src="myPic.jpg" alt="" />
             <div className='portfolio-content'>
             
-            <h4 className='hidden sm:inline-block sm:fw-bold sm:absolute sm:top-[30%] sm:left-[50%] sm:text-4xl sm:border-transparent sm:p-2 sm:text-white'>Personal Portfolio Template</h4>
-            <h5 className='hidden sm:inline-block fw-bold sm:absolute sm:top-[30%] sm:left-[50%] sm:text-4xl sm:border-transparent sm:p-2 sm:text-red-600'>Personal Portfolio Template</h5>
+            <h4 className='hidden font-serif sm:inline-block sm:font-bold sm:absolute sm:top-[30%] sm:left-[50%] sm:text-4xl sm:border-transparent sm:p-2 sm:text-white'>Personal Portfolio Template</h4>
+            <h5 className='hidden font-serif sm:inline-block font-bold sm:absolute sm:top-[30%] sm:left-[50%] sm:text-4xl sm:border-transparent sm:p-2 sm:text-red-600'>Personal Portfolio Template</h5>
 
-            <h1 className='hidden sm:inline-block absolute fw-bold top-[10%] left-[50%] text-3xl text-white'>Welcome To</h1>
-            <h2 className='hidden sm:inline-block absolute fw-bold top-[20%] left-[53%] text-3xl text-white'>Hasan's World</h2>
+            <h1 className='hidden sm:inline-block font-serif absolute font-bold top-[10%] left-[50%] text-3xl text-white'>Welcome To</h1>
+            <h2 className='hidden sm:inline-block absolute font-serif font-bold top-[20%] left-[53%] text-3xl text-white'>Hasan's World</h2>
             </div>
         </div>
         <Skills></Skills>
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,22 +2,36 @@ import React from 'react';
 
 const Skills = () => {
     return (
-        <div className='bg-gray-400'>
+        <div className='bg-gray-400 pb-5'>
         <div className=' flex justify-between'>
             
-            <div className='ml-20 mt-12'>
+            <div className='sm:ml-20 mt-12'>
             <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://lh3.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw" alt="" />
+
             <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://gbcdn.mrgcdn.ru/uploads/geekbrains/public/ckeditor_assets/pictures/8975/retina-5df8bc110901350173f8be719ce2ea1d.png" alt="" />
+
             <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://www.rlogical.com/wp-content/uploads/2020/08/icon-botsrap-1.png" alt="" />
+
             <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://d6f6d0kpz0gyr.cloudfront.net/uploads/images/_1200x630_crop_center-center_82_none/tailwind-thumb.jpg?mtime=1609771799" alt="" />
             </div>
-            <div>
-            <img className='mt-[68px] sm:w-[400px] w-[150px] ' src="https://www.interviewbit.com/blog/wp-content/uploads/2021/08/web-developer-skills-800x390.jpg" alt="" />
-            <img className='mt-[68px] sm:w-[400px] w-[150px]' src="https://www.egeniq.nl/sites/default/files/2020-06/frontend_webdeveloper.jpg" alt="" />
+            <div className='mt-5'>
+            <img className='mt-[68px] sm:mt-[50px] sm:w-[400px] sm:h-[200px] w-[100px] ' src="https://www.interviewbit.com/blog/wp-content/uploads/2021/08/web-developer-skills-800x390.jpg" alt="" />
+
+            <img className='mt-[68px] sm:mt-[50px] sm:w-[400px] sm:h-[200px] w-[100px]' src="https://www.egeniq.nl/sites/default/files/2020-06/frontend_webdeveloper.jpg" alt="" />
+            
+            <img className='mt-[68px] sm:mt-[50px] sm:w-[400px] sm:h-[200px] w-[100px]' src="https://www.pngfind.com/pngs/m/118-1181927_web-development-services-web-development-services-png-transparent.png" alt="" />
+
+            <img className='mt-[68px] sm:mt-[50px] sm:w-[400px] sm:h-[200px] w-[100px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgyIkO1RvLstJoHVPBzdMEuNqx4KyZMB3fdA&usqp=CAU" alt="" />
+            
             </div>
-            <div className='mr-20 mt-12'>
+            <div className='sm:mr-20 mt-12'>
             <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://www.computerhope.com/jargon/j/javascript.png" alt="" />
+
             <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://res.cloudinary.com/practicaldev/image/fetch/s--ljmYNm3i--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" alt="" />
+
+            <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://codesera.co.in/assets/upload/courses/USR-1615525573860.png" alt="" />
+
+            <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://dev.java/assets/images/java-logo-vert-blk.png" alt="" />
             </div>
         </div>
         </div>
