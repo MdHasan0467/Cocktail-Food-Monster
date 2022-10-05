4aae0dd056a4c11ef6b5be85b36fa009b6ce004d
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-10-05 20:47:43 +0600

    about section added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         <div className='bg-gray-400 py-4 text-gray-800 flex justify-between cursor-pointer'>
              <h2 className='sm:ml-6 sm:text-3xl bg-slate-300 py-2  rounded-full pl-3'>
              Food 
-             <span className='bg-gray-800 text-white px-3 py-1.5 rounded-full ml-2'>Monster</span></h2>
+             <span className='bg-gray-800 text-white px-3 sm:py-1.5 py-2.5 rounded-full ml-2'>Monster</span></h2>
         <div className='sm:text-2xl sm:mr-6 mr-2'>
             <Link className='sm:mr-6 py-2 sm:px-5 px-1 rounded-full hover:text-white hover:bg-gray-800' to='/home'>Home</Link>
             <Link className='sm:ml-6 py-2 sm:px-5 px-1 rounded-full hover:text-white hover:bg-gray-800' to='/meal'>Meal</Link>
---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <div className='bg-gray-400'>
+        <div className=' flex justify-between'>
+            
+            <div className='ml-20 mt-12'>
+            <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://lh3.googleusercontent.com/85WnuKkqDY4gf6tndeL4_Ng5vgRk7PTfmpI4vHMIosyq6XQ7ZGDXNtYG2s0b09kJMw" alt="" />
+            <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://gbcdn.mrgcdn.ru/uploads/geekbrains/public/ckeditor_assets/pictures/8975/retina-5df8bc110901350173f8be719ce2ea1d.png" alt="" />
+            <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://www.rlogical.com/wp-content/uploads/2020/08/icon-botsrap-1.png" alt="" />
+            <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://d6f6d0kpz0gyr.cloudfront.net/uploads/images/_1200x630_crop_center-center_82_none/tailwind-thumb.jpg?mtime=1609771799" alt="" />
+            </div>
+            <div>
+            <img className='mt-[68px] sm:w-[400px] w-[150px] ' src="https://www.interviewbit.com/blog/wp-content/uploads/2021/08/web-developer-skills-800x390.jpg" alt="" />
+            <img className='mt-[68px] sm:w-[400px] w-[150px]' src="https://www.egeniq.nl/sites/default/files/2020-06/frontend_webdeveloper.jpg" alt="" />
+            </div>
+            <div className='mr-20 mt-12'>
+            <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://www.computerhope.com/jargon/j/javascript.png" alt="" />
+            <img className = "sm:w-[250px] sm:h-[200px] mt-5 w-[130px]  h-[100px]" src="https://res.cloudinary.com/practicaldev/image/fetch/s--ljmYNm3i--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png" alt="" />
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Skills;
